a61fc8c17cdc1eb9e7e62aaa8fe2c8737526a2cd
Eduardo Lima <edulima282@gmail.com>
2022-07-12 18:24:20 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/img/icon-image.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dem-botao-vermelho">
+        <img src={icon} alt="Notificar" />
+    </div>
+    )
+}
+
+export default NotificationButton
